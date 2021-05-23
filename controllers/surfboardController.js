efd71d37a360aaf2b4641b6d2601834e5a35dc72
Tim Kelly <timpkelly1@gmail.com>
2021-05-23 12:40:30 +1000

    Created surfboard_create_post

--- controllers/surfboardController.js
+++ controllers/surfboardController.js
@@ -1,5 +1,6 @@
 const Surfboard = require('../models/Surfboard');
 const Category = require('../models/Category');
+const { body, validationResult } = require('express-validator');
 
 // Display list of all surfboards
 exports.surfboard_list = function (req, res, next) {
@@ -51,9 +52,73 @@ exports.surfboard_create_get = function (req, res, next) {
 };
 
 // Handle create surfboard form on POST
-exports.surfboard_create_post = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: create surfboard POST');
-};
+exports.surfboard_create_post = [
+  // Sanitise and validate fields
+  body('name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('You must enter a name'),
+  body('description')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('You must enter a description'),
+  body('category')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('You must enter a category'),
+  body('price')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('You must enter a price')
+    .isNumeric()
+    .withMessage('The price must be numeric'),
+  body('number_in_stock')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('You must enter a number in stock')
+    .isNumeric()
+    .withMessage('The number in stock must be numeric'),
+  function (req, res, next) {
+    const errors = validationResult(req);
+
+    // If errors, display errors with form
+    if (!errors.isEmpty()) {
+      Category.find().exec(function (err, the_categories) {
+        if (err) {
+          return next(err);
+        }
+        res.render('surfboard_form', {
+          title: 'Add New Surfboard',
+          category_list: the_categories,
+          errors: errors.array(),
+        });
+      });
+    }
+
+    // Save surfboard to db
+    const new_surfboard = new Surfboard({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      number_in_stock: req.body.number_in_stock,
+    });
+
+    new_surfboard.save(function (err, the_surfboard) {
+      if (err) {
+        return next(err);
+      }
+
+      // Redirect to surfboard
+      return res.redirect(the_surfboard.url);
+    });
+  },
+];
 
 // Display surfboard update form on GET
 exports.surfboard_update_get = function (req, res, next) {
