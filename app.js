898773f12af9657f44901771d5da6727463c268b
Your Name <you@example.com>
2021-05-24 11:47:45 +1000

    Installed and implemented dotenv

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var inventoryRouter = require('./routes/inventory');
+var indexRouter = require("./routes/index");
+var inventoryRouter = require("./routes/inventory");
 
-var compression = require('compression');
-var helmet = require('helmet');
+var compression = require("compression");
+var helmet = require("helmet");
 
+// set up dotenv
+require("dotenv").config();
+
+// set up express
 var app = express();
 
 // set up mongoose connection
-var mongoose = require('mongoose');
-var dev_db_url =
-  'mongodb+srv://timtest:test123@cluster0.eosvt.mongodb.net/surfboard_inventory?retryWrites=true&w=majority';
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
+var mongoose = require("mongoose");
+var mongoDB = process.env.MONGODB_URI || process.env.DB_LOCAL_URI;
 
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // compress responses
 app.use(compression());
@@ -29,17 +31,17 @@ app.use(compression());
 app.use(helmet());
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/inventory', inventoryRouter);
+app.use("/", indexRouter);
+app.use("/inventory", inventoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -50,11 +52,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
